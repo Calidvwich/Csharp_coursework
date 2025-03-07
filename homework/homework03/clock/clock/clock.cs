9b9d43780340ab408f380b4c0ac6c2ba35828980
ArthurXjz <147696595+Calidvwich@users.noreply.github.com>
2025-03-07 20:48:51 +0800

    20250307_01, 实现了sleep，但是蜂鸣器逻辑仍然有问题，暂时没找到好的修改方法，先行提交

---
+++ homework/homework03/clock/clock/clock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class AlarmClock
+{
+    private System.Timers.Timer timer; // 解决 CS0104 问题
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    private DateTime? alarmTime;
+
+    public AlarmClock()
+    {
+        timer = new System.Timers.Timer(1000); // 每秒触发一次
+        timer.Elapsed += OnTick;
+    }
+
+    public void SetAlarm(DateTime time)
+    {
+        if (time < DateTime.Now)
+        {
+            throw new ArgumentException("闹钟时间不能设置为过去的时间！");
+        }
+        alarmTime = time;
+        Console.WriteLine($"闹钟已设置：{time:HH:mm:ss}");
+    }
+
+    public void Start()
+    {
+        timer.Start();
+        Console.WriteLine("闹钟已启动...");
+    }
+
+    public void Stop()
+    {
+        timer.Stop();
+        Console.WriteLine("闹钟已停止...");
+    }
+
+    private void OnTick(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+        Console.WriteLine($"当前时间：{DateTime.Now:HH:mm:ss}");
+
+        if (alarmTime.HasValue && DateTime.Now >= alarmTime.Value)
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+            Stop(); // 闹钟响后自动停止
+        }
+    }
+}
---
+++ homework/homework03/clock/clock/program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace MyAlarmApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            AlarmClock clock = new AlarmClock();
+
+            clock.Tick += (sender, e) =>
+            {
+                // 这里打印每秒的时间
+                Console.WriteLine($"Tick: 当前时间是 {DateTime.Now:HH:mm:ss}");
+            };
+
+            clock.Alarm += (sender, e) =>
+            {
+                // 触发响铃时打印提示信息
+                Console.WriteLine("时间到了！");
+                Console.Beep();  // 播放系统蜂鸣声
+            };
+
+            try
+            {
+                Console.Write("请输入闹钟时间（格式 HH:mm:ss）：");
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime alarmTime))
+                {
+                    if (alarmTime < DateTime.Now)
+                    {
+                        Console.WriteLine("不能设置过去的时间");
+                    }
+                    else
+                    {
+                        clock.SetAlarm(alarmTime);  // 设置闹钟时间
+                        clock.Start();               // 启动计时器
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("输入格式错误");
+                }
+
+                // 等待直到用户按下任意键，防止程序提前退出
+                Console.WriteLine("按任意键退出程序...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"发生错误：{ex.Message}");
+            }
+        }
+    }
+}
+
